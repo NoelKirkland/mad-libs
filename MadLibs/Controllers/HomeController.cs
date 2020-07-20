1b47151b315d12826b97a46cad66d881187b0557
Erich Richter <erich.richter@gmail.com>
2020-07-20 14:51:46 -0700

    add unique forms corresponding to user selection at home page > > Co-Authored-By: Taylor Phillips <taylorphillips133@gmail.com> Co-authored-by: Noel Kirkland <noelkirkland@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -7,10 +7,16 @@ namespace MadLib.Controllers
   {
 
     [Route("/")]
-    public ActionResult Form() { return View(); }
+    public ActionResult FormGate() { return View(); }
 
-    [Route("/story")]
-    public ActionResult Story(string person1, string person2, string animal, string exclamation, string verb, string noun)
+    [Route("/form1")]
+    public ActionResult Form1() { return View(); }
+
+    [Route("/form2")]
+    public ActionResult Form2() { return View(); }
+
+    [Route("/story1")]
+    public ActionResult Story1(string person1, string person2, string animal, string exclamation, string verb, string noun)
     {
     MadLibVariable newMadLib = new MadLibVariable();
     newMadLib.Person1 = person1;
@@ -20,6 +26,19 @@ namespace MadLib.Controllers
     newMadLib.Verb = verb;
     newMadLib.Noun = noun;
     return View(newMadLib);
-}
+    }
+
+    [Route("/story2")]
+    public ActionResult Story2(string person1, string person2, string animal, string exclamation, string verb, string noun)
+    {
+    MadLibVariable newMadLib = new MadLibVariable();
+    newMadLib.Person1 = person1;
+    newMadLib.Person2 = person2;
+    newMadLib.Animal = animal;
+    newMadLib.Exclamation = exclamation;
+    newMadLib.Verb = verb;
+    newMadLib.Noun = noun;
+    return View(newMadLib);
+    }
   }
 }
