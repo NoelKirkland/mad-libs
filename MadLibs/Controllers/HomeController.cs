e685a59d8e02c6dc0fa80502ee716abad6593df5
Erich Richter <erich.richter@gmail.com>
2020-07-20 15:36:42 -0700

    add functionality form multiple forms/pages > > Co-Authored-By: Taylor Phillips <taylorphillips133@gmail.com> Co-authored-by: Noel Kirkland <noelkirkland@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace MadLib.Controllers
     [Route("/story1")]
     public ActionResult Story1(string person1, string person2, string animal, string exclamation, string verb, string noun)
     {
-    MadLibVariable newMadLib = new MadLibVariable();
+    MadLibVariable1 newMadLib = new MadLibVariable1();
     newMadLib.Person1 = person1;
     newMadLib.Person2 = person2;
     newMadLib.Animal = animal;
@@ -29,15 +29,23 @@ namespace MadLib.Controllers
     }
 
     [Route("/story2")]
-    public ActionResult Story2(string person1, string person2, string animal, string exclamation, string verb, string noun)
+    public ActionResult Story2(string adjective1, string adjective2, string bird, string room, string verb1, string verb2, string relative, string noun, string drink, string verbIng1, string bodyPart, string nounPlural, string verbIng2, string noun2)
     {
-    MadLibVariable newMadLib = new MadLibVariable();
-    newMadLib.Person1 = person1;
-    newMadLib.Person2 = person2;
-    newMadLib.Animal = animal;
-    newMadLib.Exclamation = exclamation;
-    newMadLib.Verb = verb;
+    MadLibVariable2 newMadLib = new MadLibVariable2();
+    newMadLib.Adjective1= adjective1;
+    newMadLib.Adjective2 = adjective2;
+    newMadLib.Bird = bird;
+    newMadLib.Room = room;
+    newMadLib.Verb1 = verb1;
+    newMadLib.Verb2 = verb2;
+    newMadLib.Relative = relative;
     newMadLib.Noun = noun;
+    newMadLib.Drink = drink;
+    newMadLib.VerbIng1 = verbIng1;
+    newMadLib.BodyPart = bodyPart;
+    newMadLib.NounPlural = nounPlural;
+    newMadLib.VerbIng2 = verbIng2;
+    newMadLib.Noun2 = noun2;
     return View(newMadLib);
     }
   }
--- MadLibs/Models/MadLibVariables.cs
+++ MadLibs/Models/MadLibVariables.cs
@@ -1,6 +1,6 @@
 namespace MadLib.Models
 {
-  public class MadLibVariable
+  public class MadLibVariable1
   {
     public string Person1 { get; set; }
 
@@ -13,5 +13,36 @@ namespace MadLib.Models
     public string Exclamation  { get; set; }
 
     public string Animal  { get; set; }
+  }
+
+  public class MadLibVariable2
+  {
+    public string Adjective1 { get; set; }
+
+    public string Adjective2 { get; set; }
+
+    public string Bird { get; set; }
+    
+    public string Room { get; set; }
+
+    public string Verb1 { get; set; }
+
+    public string Verb2 { get; set; }
+
+    public string Relative { get; set; }
+
+    public string Noun { get; set; }
+
+    public string Drink { get; set; }
+
+    public string VerbIng1 { get; set; }
+
+    public string BodyPart { get; set; }
+
+    public string NounPlural { get; set; }
+
+    public string VerbIng2 { get; set; }
+
+    public string Noun2 { get; set; }
   }
 }
